9b7b8aa429d205028c0867e39322ca8484318aa4
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-04-20 23:25:33 +0530

    my first program in CLion

---
+++ ash.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout << "Enter the number of time you want to print it:" << endl;
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        cout << "Ramalund Pandey" << endl;
+    }
+}
